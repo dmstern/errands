3d7ae6a092b0fbcf916be57e80be89a0a4652911
ms <ms@swe-mbp7.local>
2014-05-14 11:55:26 +0200

    Button-Style angepasst: Alle runde Ecken

--- www/js/style.js
+++ www/js/style.js
@@ -9,18 +9,20 @@ var domEditor = {
 	 * definieren.
 	 */
 	applyStyles : function() {
+
 		$('.add-button')
 				.addClass(
-						"ui-btn ui-icon-plus ui-btn-icon-left ui-btn-inline ui-corner-all ui-shadow");
+						"ui-btn ui-icon-plus ui-btn-icon-left ui-btn-inline ui-shadow");
 		$('.close-button')
 				.addClass(
-						"ui-btn ui-icon-delete ui-btn-icon-left ui-btn-inline ui-shadow ui-corner-all");
+						"ui-btn ui-icon-delete ui-btn-icon-left ui-btn-inline ui-shadow");
 		$('.ok-button')
 				.addClass(
-						"ui-btn ui-icon-check ui-btn-icon-left ui-shadow ui-corner-all");
+						"ui-btn ui-icon-check ui-btn-icon-left ui-shadow");
 		$('.overlay-panel')
 				.addClass(
 						"ui-panel ui-panel-position-right ui-panel-display-overlay ui-body-b ui-panel-animate ui-panel-open");
+		$('[data-role=button]').addClass("ui-corner-all");
 
 		console.log("Applying custom styles...");
 	}
