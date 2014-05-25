a299bed20c860a08a21d106b982c6eb6fa3d2b04
dmstern <morgenstern.daniel@gmail.com>
2014-05-26 00:34:45 +0200

    viewModel global gemacht.

--- www/js/index.js
+++ www/js/index.js
@@ -2,14 +2,15 @@
  * Main start script for the app.
  */
 app = {
+	viewModel : null,
 	initialize : function() {
 		
 		// Initialize Model
 		model.initialize();
 		
 		// Initialize ViewModel with test data
-		var viewModel = new ErrandsViewModel(util.getTestLists());
-		ko.applyBindings(viewModel);
+		this.viewModel = new ErrandsViewModel(util.getTestLists());
+		ko.applyBindings(this.viewModel);
 		console.log("Applying DataBindings...");
 
 		// Apply custom styles
